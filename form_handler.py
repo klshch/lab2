f3531bc74dd4b9d1ee09d978755b71ec9cbe047c
Anastasiia Klishch <147326209+klshch@users.noreply.github.com>
2023-10-12 01:51:40 +0300

    task2

--- form_handler.py
+++ form_handler.py
@@ -20,11 +20,29 @@ try:
     for interest in interests:
         value_choice = form.getvalue(interest, default="off")
         interests_checkbox[interest] = value_choice
+
+    print(f"Set-Cookie: name={name};")
+    print(f"Set-Cookie: lastname={lastname};")
     
 
 except Exception as e:
     print(f"Content-type:text/html\r\n\r\n")
     print(f"Error: {str(e)}")
+
+
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+name_cookie = cookie.get("name").value
+lastname_cookie = cookie.get("lastname").value
+
+count_cookie = int(cookie.get("count_cookie").value) if "count_cookie" in cookie else 0
+count_cookie += 1
+
+print(f"Set-Cookie: count_cookie={count_cookie};")
+
+if "delete_cookies" in form:
+    print("Set-Cookie: name_cookie=" "; expires=Thu, 01 Jan 1970 00:00:00 GMT;")
+    print("Set-Cookie: lastname_cookie=" "; expires=Thu, 01 Jan 1970 00:00:00 GMT;")
+    print("Set-Cookie: count_cookie=" "; expires=Thu, 01 Jan 1970 00:00:00 GMT;")
 
 
 print("Content-type:text/html\r\n\r\n")
@@ -64,6 +82,19 @@ template_html = f"""
         <br>
         <b>Thank you for filling out the form!</b>
         <br>
+        <br>
+        <br>
+        <h2>From cookie:</h2>
+        Name - {name_cookie} Lastname - {lastname_cookie} 
+        <br>
+        Number of submitted forms: {count_cookie}
+        <br>
+        If you want you can delete all cookies: 
+        <br>
+        <form action=" " method="post">
+            <input type="hidden" name="delete_cookies" value="1">
+            <input type="submit" value="Delete">
+        </form>
     </div>
 </body>
 </html>
